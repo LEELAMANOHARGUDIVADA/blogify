b76556df7957b45d75ef1d39908b0eee251703b4
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-06-01 03:00:19 +0530

    Update blog.controller.js

--- server/controllers/blog.controller.js
+++ server/controllers/blog.controller.js
@@ -15,6 +15,7 @@ const getAllBlogs = async (req, res) => {
       blogs,
     });
   } catch (error) {
+    console.log(error);
     return res
       .status(500)
       .json({ message: "Error While Fetching Blogs", error });
