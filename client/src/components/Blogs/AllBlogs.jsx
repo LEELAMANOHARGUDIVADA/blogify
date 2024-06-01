5a826daf8e9f2ec7566d21f78f3080bfb59d3c8d
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-06-01 15:18:52 +0530

    Update AllBlogs.jsx

--- client/src/components/Blogs/AllBlogs.jsx
+++ client/src/components/Blogs/AllBlogs.jsx
@@ -9,7 +9,7 @@ const AllBlogs = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const response = await fetch("https://blogify-api-neon.vercel.app/api/blog/getallblogs");
+        const response = await fetch("https://blogify-api-kohl.vercel.app/api/blog/getallblogs");
         if (!response.ok) {
           throw  new Error("Cannot fetch Blogs")
         }
