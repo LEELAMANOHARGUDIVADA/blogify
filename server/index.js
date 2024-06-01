34b5c02432ae1ae217843d04eca2a5223ac02c8b
LEELA MANOHAR GUDIVADA <142682424+LEELAMANOHARGUDIVADA@users.noreply.github.com>
2024-06-01 13:40:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://blogify-seven-hazel.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    origin: "https://blogify-seven-hazel.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
